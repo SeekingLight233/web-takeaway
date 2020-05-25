6602739a2ab9c1792b35624eb47de52f91fe55f2
Jason <690163223@qq.com>
2020-05-25 13:29:21 +0800

    先删除redux-dev插件

--- src/pages/index/store.js
+++ src/pages/index/store.js
@@ -1,19 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import mainReducer from "./reducers/main";
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import mainReducer from "./reducers/main"
 
-const store = createStore(
-  mainReducer,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(mainReducer, applyMiddleware(thunk))
 
 //为store配置热更新
 if (module.hot) {
   module.hot.accept("./reducers/main", () => {
-    const nextRootReducer = require("./reducers/main").default;
-    store.replaceReducer(nextRootReducer);
-  });
+    const nextRootReducer = require("./reducers/main").default
+    store.replaceReducer(nextRootReducer)
+  })
 }
 
-export default store;
+export default store
