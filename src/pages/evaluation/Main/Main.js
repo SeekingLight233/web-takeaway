c923abb182ffb6ea211a151681375f2920c70301
SeekingLight233 <690163223@qq.com>
2020-06-19 13:33:01 +0800

    feat: 完成评价逻辑

--- src/pages/evaluation/Main/Main.js
+++ src/pages/evaluation/Main/Main.js
@@ -7,38 +7,40 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    // 最多可输入的字符数
+    // 最多还能输入多少个字符
     this.maxCount = 140;
 
     this.state = {
       // 还剩多少字符可输入
       count: this.maxCount,
-      // 用户当前点击的星号码
+      // 用户点击的星星下标值
       startIndex: 0,
     };
   }
-  //   componentDidMount() {
-  //     this.commentInput.addEventListener("compositionstart", () => {
-  //       this.chineseInputing = true;
-  //     });
-  //     this.commentInput.addEventListener("compositionend", (e) => {
-  //       this.chineseInputing = false;
-  //       this.onIuput(e.target.value);
-  //     });
-  //   }
+  componentDidMount() {
+    //为textarea监听一下拼写缓冲区
+    this.commentInput.addEventListener("compositionstart", () => {
+      this.chineseInputing = true;
+    });
+    this.commentInput.addEventListener("compositionend", (e) => {
+      this.chineseInputing = false;
+      this.onIuput(e.target.value);
+    });
+  }
   /**
-   * 用户输入回调
+   * 用户输入回调，动态更改剩余字符数
    */
   onIuput(value) {
-    let num = value.length;
+    let num = value.length; //取出当前输入的字符
     if (!this.chineseInputing) {
+      //拼写监听
       this.setState({
         count: this.maxCount - num,
       });
     }
   }
   /**
-   * 点击评分
+   * 点击哪个就把索引设置为哪个
    */
   doEva(i) {
     this.setState({
@@ -51,6 +53,7 @@ class Main extends React.Component {
   renderStar() {
     let array = [];
     for (let i = 0; i < 5; i++) {
+      //如果当前星星的索引在被点击星星的左边，就设置为暗色，否则设置为亮色
       let cls = i >= this.state.startIndex ? "star-item" : "star-item light";
       array.push(
         <div onClick={() => this.doEva(i)} key={i} className={cls}></div>
@@ -70,6 +73,7 @@ class Main extends React.Component {
               ref={(ref) => {
                 this.commentInput = ref;
               }}
+              //将textarea中的值传递过去
               onChange={(e) => this.onIuput(e.target.value)}
               minLength="140"
               placeholder="亲，菜品的口味如何，商家的服务是否周到?"
