5c3fd80821054ca7ff1fab3aeed9bdc0f9fcc2f6
SeekingLight233 <690163223@qq.com>
2020-06-21 19:36:20 +0800

    feat:评价顶部

--- src/component/ListItem/ListItem.js
+++ src/component/ListItem/ListItem.js
@@ -2,6 +2,7 @@ import "./ListItem.scss";
 import React from "react";
 import { connect } from "react-redux";
 // import { getListData } from "../../actions/contentListAction"
+import StarScore from "component/StarScore/StarScore";
 /**
  * @description  列表单个组件
  */
@@ -20,36 +21,6 @@ class ListItem extends React.Component {
     }
   }
 
-  /**
-   * @description 渲染评分的星星
-   * @param {*} data
-   */
-  renderScore(data) {
-    let wm_poi_score = data.wm_poi_score || "";
-    //传进来的是number类型
-    let score = wm_poi_score.toString();
-    let scoreArray = score.split(".");
-    let fullstar = parseInt(scoreArray[0]); //满星的个数
-    //对半颗星进行四舍五入
-    let halfstar = parseInt(scoreArray[1] >= 5 ? 1 : 0);
-    let nullstar = 5 - fullstar - halfstar;
-
-    let starjsx = [];
-    for (let i = 0; i < fullstar; i++) {
-      starjsx.push(<div key={i + "full"} className="star fullstar"></div>);
-    }
-    if (halfstar) {
-      for (let j = 0; j < halfstar; j++) {
-        starjsx.push(<div key={j + "half"} className="star halfstar"></div>);
-      }
-    }
-    if (nullstar) {
-      for (let k = 0; k < nullstar; k++) {
-        starjsx.push(<div key={k + "null"} className="star nullstar"></div>);
-      }
-    }
-    return starjsx;
-  }
   /**
    * @description 渲染月售数量
    * @param {*} data
@@ -99,7 +70,9 @@ class ListItem extends React.Component {
           <p className="item-title">{data.name}</p>
           {/* 清除浮动，防止父元素坍塌 */}
           <div className="item-desc clearfix">
-            <div className="item-score">{this.renderScore(data)}</div>
+            <div className="item-score">
+              <StarScore score={data.wm_poi_score}></StarScore>
+            </div>
             <div className="item-count">月售{this.renderMonthNum(data)}</div>
             <div className="item-distance">&nbsp;{data.distance}</div>
             <div className="item-time">{data.mt_delivery_time}&nbsp;|</div>
--- src/pages/detail/Comment/Comment.js
+++ src/pages/detail/Comment/Comment.js
@@ -4,7 +4,7 @@ import React from "react";
 
 import { connect } from "react-redux";
 
-// import StarScore from 'component/StarScore/StarScore';
+import StarScore from "component/StarScore/StarScore";
 // import CommentList from './CommentList/CommentList';
 // import { getListData } from '../actions/commentAction';
 
@@ -19,8 +19,39 @@ class Comment extends React.Component {
   }
 
   render() {
-    let data = this.props.commentData;
-    return <div className="comment-inner">Comment</div>;
+    // let data = this.props.commentData;
+    return (
+      <div className="comment-inner">
+        <div className="comment-score">
+          <div className="mail-score-content">
+            <div className="mail-score">5.0</div>
+            <div className="mail-text">商家评价</div>
+          </div>
+
+          <div className="other-score-content">
+            <div className="taste-score">
+              <div className="taste-text">口味</div>
+              <div className="taste-star-wrap">
+                <StarScore score={4.5}></StarScore>
+              </div>
+              <div className="taste-score-text">4.7</div>
+            </div>
+
+            <div className="package-score">
+              <div className="package-text">包装</div>
+              <div className="package-star-wrap">
+                <StarScore score={4.5}></StarScore>
+              </div>
+              <div className="package-score-text">4.2</div>
+            </div>
+          </div>
+          <div className="send-score-content">
+            <div className="send-score">4.9</div>
+            <div className="send-text">配送评价</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
